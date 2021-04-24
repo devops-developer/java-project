4a3252cf2d308122d9868765550765b4ed26763e
root <root@ip-172-31-32-231.ap-south-1.compute.internal>
2021-04-24 07:13:45 +0000

    java-app

--- helloworld.java
+++ helloworld.java
@@ -4,8 +4,6 @@ public class HelloWorld {
     System.out.println("Happy Ugadi.....");
     System.out.println("Happy Ugadi.....");
     System.out.println("Happy Ugadi.....");
-    System.out.println("Happy Ugadi.....");
-    System.out.println("Happy Ugadi.....");
     System.out.println("Happy Ugadi.....bbncn");
         System.out.println("hello guys enter into devops real world");
 
