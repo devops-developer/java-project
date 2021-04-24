91f60fe8e0b00622a2981d64eee85465cb6d9d1d
devops-developer <83109540+devops-developer@users.noreply.github.com>
2021-04-24 12:19:28 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Happy sankranti");
+    System.out.println("Happy Ugadi.....");
+    System.out.println("Happy Ugadi.....");
+    System.out.println("Happy Ugadi.....");
+    System.out.println("Happy Ugadi.....");
+    System.out.println("Happy Ugadi.....");
+    System.out.println("Happy Ugadi.....bbncn");
+        System.out.println("hello guys enter into devops real world");
+
+  }
+}
